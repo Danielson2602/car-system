12947339698ced85c48b7d8105d9a926224d8ef9
danielson.augusto <danielson2602@gmail.com>
2017-05-20 15:22:19 -0300

    projeto

---
+++ car-system-ejb/src/main/java/LicencasLiberadas.java
@@ -0,0 +1,10 @@
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "licencas_liberadas", schema = "car-system")
+public class LicencasLiberadas implements Serializable{
+
+
+}
